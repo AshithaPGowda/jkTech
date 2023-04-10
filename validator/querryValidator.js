fd7496b695528b88efed8c26bfa612a61f351c97
Ashitha P Gowda <ashithapgowdaapg@gmail.com>
2023-04-10 10:44:39 +0530

    next added in checkQueryParams

--- validator/querryValidator.js
+++ validator/querryValidator.js
@@ -1,12 +1,11 @@
 function checkQueryParams(req, res, next) {
+    console.log("in check Querry Param ")
     const query = req.query;
-  
     for (const param in query) {
       if (!query[param]) {
         return res.status(400).send(`Missing value for ${param}`);
       }
     }
-    return true
+    next()
 }
-
 module.exports = {checkQueryParams};
