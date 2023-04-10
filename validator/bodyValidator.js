74a83e4af8b39b97dfab7da0994d1b6d912a304e
Ashitha P Gowda <ashithapgowdaapg@gmail.com>
2023-04-10 10:44:58 +0530

    formated document

--- validator/bodyValidator.js
+++ validator/bodyValidator.js
@@ -1,12 +1,10 @@
 function checkBodyParams(req, res, next) {
     const body = req.body;
-  
     for (const param in body) {
       if (!body[param]) {
         return res.status(400).send(`Missing value for ${param}`);
       }
     }
-  
     next();
 }
 
