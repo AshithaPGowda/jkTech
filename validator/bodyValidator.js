68ad82bd40751d6a54337f3c8e385ea1f0d83376
Ashitha P Gowda <ashithapgowdaapg@gmail.com>
2023-04-10 09:56:22 +0530

    added a middleware to check if any body params are empty

---
+++ validator/bodyValidator.js
@@ -0,0 +1,13 @@
+function checkBodyParams(req, res, next) {
+    const body = req.body;
+  
+    for (const param in body) {
+      if (!body[param]) {
+        return res.status(400).send(`Missing value for ${param}`);
+      }
+    }
+  
+    next();
+}
+
+module.exports = checkBodyParams;
